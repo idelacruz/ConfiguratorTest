57f660572e499efbb5dc3bc46b10f1326272fe44
idelacruz <ignacio.delacruz.g@gmail.com>
2022-03-31 12:43:01 +0200

    lavabo

---
+++ TestLavabo/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1376015.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 75233108, 75233094, 75233089, 75233109, 75244640, 75244702, 75295722, 75296422, 75297061 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
