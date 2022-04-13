8aad6be821697a7a03f4ed1a7f9540ed0efbb9ae
idelacruz <ignacio.delacruz.g@gmail.com>
2022-04-13 18:54:16 +0200

    confV1

---
+++ ConfiguradorV1/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1387774.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 77252118, 77187504, 77187515, 77187508, 77187503, 77188747, 77196081, 77252119, 77252244, 77253968, 77294655, 77252123, 77348215, 77354474, 77354756, 77544589, 77600258 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
