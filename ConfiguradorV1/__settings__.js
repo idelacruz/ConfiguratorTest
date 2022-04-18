152c725af152d5f3f6b53e8f0c46b7dbab80e21f
idelacruz <ignacio.delacruz.g@gmail.com>
2022-04-18 17:30:44 +0200

    Responsive

--- ConfiguradorV1/__settings__.js
+++ ConfiguradorV1/__settings__.js
@@ -3,12 +3,12 @@ SCRIPT_PREFIX = "";
 SCENE_PATH = "1387774.json";
 CONTEXT_OPTIONS = {
     'antialias': true,
-    'alpha': false,
+    'alpha': true,
     'preserveDrawingBuffer': false,
     'preferWebGl2': true,
     'powerPreference': "default"
 };
-SCRIPTS = [ 77252118, 77187504, 77187515, 77187508, 77187503, 77188747, 77196081, 77252119, 77252244, 77253968, 77294655, 77252123, 77348215, 77354474, 77354756, 77544589, 77600258, 77610719 ];
+SCRIPTS = [ 77252118, 77187504, 77187515, 77187508, 77187503, 77188747, 77252119, 77252244, 77253968, 77294655, 77252123, 77348215, 77354474, 77354756, 77544589, 77600258, 77610719, 78204687, 78268297 ];
 CONFIG_FILENAME = "config.json";
 INPUT_SETTINGS = {
     useKeyboard: true,
@@ -18,4 +18,5 @@ INPUT_SETTINGS = {
 };
 pc.script.legacy = false;
 PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/78268212/1/basis.wasm.js', 'wasmUrl' : 'files/assets/78268213/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/78268211/1/basis.js', 'preload' : false},
 ];
